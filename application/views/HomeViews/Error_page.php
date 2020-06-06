2a56473cb8c2c9b776e6930a00fcf6e788c9d532
Afsara Yeasmin <afsarayeasmin30@gmail.com>
2020-06-06 16:47:33 +0530

    Add new

--- application/views/HomeViews/Error_page.php
+++ application/views/HomeViews/Error_page.php
@@ -3,10 +3,10 @@
   <div class="row" style="padding:12% 0px;">
     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">&nbsp;</div>
   <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-    <h4>Oops! Page Not Found</h4>
+    <h4 style="font-size: 27px;padding: 0px 0px 0px 15px;">Oops! Page Not Found</h4>
     <h3 class="error-page-h3">404</h3>
-    <p>We can't find the page you are looking for</p>
-    <a href="#">Go Back Home</a>
+    <p style="padding: 0px 0px 0px 10px;">We can't find the page you are looking for</p>
+    <a href="#" class="error-page-a">Go Back Home</a>
   </div>
 </div>
 </div>
